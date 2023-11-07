9b95c38cf5838a72bd394027abb7a5160fa21add
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-11-07 21:09:27 +0100

    mm

--- Context/ApplicationContext.cs
+++ Context/ApplicationContext.cs
@@ -8,50 +8,37 @@ using Microsoft.EntityFrameworkCore;
 
 namespace CMS_appBackend.Context
 {
-    public class ApplicationContext : DbContext
+    public class ApplicationContext : DbContext 
     {
-        public ApplicationContext(DbContextOptions<ApplicationContext> options)
-            : base(options) { }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public ApplicationContext (DbContextOptions<ApplicationContext> options): base(options)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-                // Use environment variables to set the connection string
-                string host = Environment.GetEnvironmentVariable("Render_PostgreSQL_Host");
-                string port = Environment.GetEnvironmentVariable("Render_PostgreSQL_Port");
-                string database = Environment.GetEnvironmentVariable("Render_PostgreSQL_Database");
-                string username = Environment.GetEnvironmentVariable("Render_PostgreSQL_Username");
-                string password = Environment.GetEnvironmentVariable("Render_PostgreSQL_Password");
 
-                if (
-                    string.IsNullOrEmpty(host)
-                    || string.IsNullOrEmpty(port)
-                    || string.IsNullOrEmpty(database)
-                    || string.IsNullOrEmpty(username)
-                    || string.IsNullOrEmpty(password)
-                )
-                {
-                    throw new InvalidOperationException(
-                        "PostgreSQL connection information not found in environment variables."
-                    );
-                }
-
-                string connectionString =
-                    $"Host={host};Port={port};Database={database};Username={username};Password={password}";
-
-                optionsBuilder.UseNpgsql(connectionString);
-            }
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            // Use environment variables to set the connection string
+            string host = Environment.GetEnvironmentVariable("Render_PostgreSQL_Host");
+            string port = Environment.GetEnvironmentVariable("Render_PostgreSQL_Port");
+            string database = Environment.GetEnvironmentVariable("Render_PostgreSQL_Database");
+            string username = Environment.GetEnvironmentVariable("Render_PostgreSQL_Username");
+            string password = Environment.GetEnvironmentVariable("Render_PostgreSQL_Password");
 
+            string connectionString = $"Host={host};Port={port};Database={database};Username={username};Password={password}";
+
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
         public DbSet<User> Users { get; set; }
         public DbSet<Admin> Admins { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
-        public DbSet<Role> Roles { get; set; }
+        public DbSet<Role> Roles{get;set;}
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Comment> Comments { get; set; }
-        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Comment> Comments{get;set;}
+        public DbSet<Customer> Customers{get;set;}
+        
     }
 }
