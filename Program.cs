9300310859d24fb2219f1a7540fa7ed87d7d96e9
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-11-18 17:30:46 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -106,25 +106,3 @@ using (var scope = app.Services.CreateScope())
     await initializer.Initialize().ConfigureAwait(false);
 }
 app.Run();
-
-// builder.Services.AddScoped<IAdminService, AdminService>();
-// builder.Services.AddScoped<IAdminRepository, AdminRepository>();
-
-// builder.Services.AddScoped<IBlogService, BlogService>();
-// builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-
-// builder.Services.AddScoped<ICommentService, CommentService>();
-// builder.Services.AddScoped<ICommentRepository, CommentRepository>();
-
-// builder.Services.AddScoped<ICategoryService, CategoryService>();
-// builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-
-// builder.Services.AddScoped<IPostService, PostService>();
-// builder.Services.AddScoped<IPostRepository, PostRepository>();
-
-// builder.Services.AddScoped<IUserService, UserService>();
-// builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-
-builder.Services.AddScoped<IRoleService, RoleService>();
-builder.Services.AddScoped<IRoleRepository, RoleRepository>();
