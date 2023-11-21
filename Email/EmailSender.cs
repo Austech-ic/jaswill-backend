77faa062fe3a2d88e3816b6ec8886bdc1aba5c1b
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-11-21 01:07:46 +0100

    updates

--- Email/EmailSender.cs
+++ Email/EmailSender.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using sib_api_v3_sdk.Api;
+using sib_api_v3_sdk.Client;
+using sib_api_v3_sdk.Model;
+using System;
+using System.Diagnostics;
+using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.Configuration;
 
 namespace CMS_appBackend.Email
 {
@@ -21,23 +22,61 @@ namespace CMS_appBackend.Email
             var brevoApiKey = _configuration["Brevo:api-key"];
             var brevoApiUrl = _configuration["Brevo:api-url"];
 
-            using (var client = new HttpClient())
-            {
-                client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", brevoApiKey);
+            var apiInstance = new TransactionalEmailsApi();
+            string SenderName = "Jaswill Real Estate";
+            string SenderEmail = "admin@jaswill.com";
+            SendSmtpEmailSender Email = new SendSmtpEmailSender(SenderName, SenderEmail);
+            string ToEmail = email.ReceiverEmail;
+            string ToName = email.ReceiverName;
+            SendSmtpEmailTo smtpEmailTo = new SendSmtpEmailTo(ToEmail, ToName);
+            List<SendSmtpEmailTo> To = new List<SendSmtpEmailTo>();
+            To.Add(smtpEmailTo);
+            string BccName = "Janice Doe";
+            string BccEmail = "example2@example2.com";
+            SendSmtpEmailBcc BccData = new SendSmtpEmailBcc(BccEmail, BccName);
+            List<SendSmtpEmailBcc> Bcc = new List<SendSmtpEmailBcc>();
+            Bcc.Add(BccData);
+            string CcName = "John Doe";
+            string CcEmail = "example3@example2.com";
+            SendSmtpEmailCc CcData = new SendSmtpEmailCc(CcEmail, CcName);
+            List<SendSmtpEmailCc> Cc = new List<SendSmtpEmailCc>();
+            Cc.Add(CcData);
+            string HtmlContent = $"<html><body><h6>{email.Message}</h6></body></html>";
+            string TextContent = null;
+            string Subject = email.Subject;
+            string ReplyToName = "Jaswill Real Estate";
+            string ReplyToEmail = "admin@jaswill.com";
+            SendSmtpEmailReplyTo ReplyTo = new SendSmtpEmailReplyTo(ReplyToEmail, ReplyToName);
+            string AttachmentUrl = null;
+            string stringInBase64 = "aGVsbG8gdGhpcyBpcyB0ZXN0";
+            byte[] Content = System.Convert.FromBase64String(stringInBase64);
+            string AttachmentName = "test.txt";
+            SendSmtpEmailAttachment AttachmentContent = new SendSmtpEmailAttachment(AttachmentUrl, Content, AttachmentName);
+            List<SendSmtpEmailAttachment> Attachment = new List<SendSmtpEmailAttachment>();
+            Attachment.Add(AttachmentContent);
+            JObject Headers = new JObject();
+            Headers.Add("Some-Custom-Name", "unique-id-1234");
+            long? TemplateId = null;
+            JObject Params = new JObject();
+            Params.Add("parameter", "My param value");
+            Params.Add("subject", "New Subject");
+            List<string> Tags = new List<string>();
+            Tags.Add("mytag");
+            SendSmtpEmailTo1 smtpEmailTo1 = new SendSmtpEmailTo1(ToEmail, ToName);
+            List<SendSmtpEmailTo1> To1 = new List<SendSmtpEmailTo1>();
+            To1.Add(smtpEmailTo1);
+            var g = Guid.NewGuid().ToString();
+            Dictionary<string, object> _parmas = new Dictionary<string, object>();
+            _parmas.Add(g, Params);
+            SendSmtpEmailReplyTo1 ReplyTo1 = new SendSmtpEmailReplyTo1(ReplyToEmail, ReplyToName);
+            SendSmtpEmailMessageVersions messageVersion = new SendSmtpEmailMessageVersions(To1, _parmas, Bcc, Cc, ReplyTo1, Subject);
+            List<SendSmtpEmailMessageVersions> messageVersiopns = new List<SendSmtpEmailMessageVersions>();
+            messageVersiopns.Add(messageVersion);
 
-                var form = new Dictionary<string, string>
-                {
-                    { "from", "Jaswill Real Estate <aderibigbeolamide56@gmail.com>" },
-                    { "to", $"{email.ReceiverName} <{email.ReceiverEmail}>" },
-                    { "subject", email.Subject },
-                    { "html", $"<html><body><h6>{email.Message}</h6></body></html>" }
-                    // Add other Brevo-specific parameters as needed
-                };
-
-                var response = await client.PostAsync(brevoApiUrl, new FormUrlEncodedContent(form));
-
-                return response.IsSuccessStatusCode;
-            }
+            var sendSmtpEmail = new SendSmtpEmail(Email, To, Bcc, Cc, HtmlContent, TextContent, Subject, ReplyTo, Attachment, Headers, TemplateId, Params, messageVersiopns, Tags);
+            CreateSmtpEmail result = apiInstance.SendTransacEmail(sendSmtpEmail);
+            Configuration.Default.ApiKey.Clear();
+            return true;
         }
     }
 }
