29e0cf99b1155928c14ff8fc59304772a7bc356f
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-11-21 05:11:36 +0100

    updates

--- Program.cs
+++ Program.cs
@@ -14,65 +14,10 @@ using System.Data;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddCors(
-    a =>
-        a.AddPolicy(
-            "CorsPolicy",
-            b =>
-            {
-                b.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
-            }
-        )
-);
-
-builder.Services.AddScoped<IAdminService, AdminService>();
-builder.Services.AddScoped<IAdminRepository, AdminRepository>();
-
-builder.Services.AddScoped<IBlogService, BlogService>();
-builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-
-builder.Services.AddScoped<ICommentService, CommentService>();
-builder.Services.AddScoped<ICommentRepository, CommentRepository>();
-
-builder.Services.AddScoped<ICategoryService, CategoryService>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
-
-builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
-builder.Services.AddScoped<ICustomerService, CustomerService>();
-
-builder.Services.AddScoped<IPostService, PostService>();
-builder.Services.AddScoped<IPostRepository, PostRepository>();
-
-builder.Services.AddScoped<IUserService, UserService>();
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-
-builder.Services.AddScoped<IRoleService, RoleService>();
-builder.Services.AddScoped<IRoleRepository, RoleRepository>();
-
-builder.Services.AddScoped<IEmailSender, EmailSender>();
-
-builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
-builder.Services.AddScoped<DatabaseInitializer>();
-builder.Services.AddHttpContextAccessor();
 
 // Add services to the container.
+ConfigureServices(builder.Services, builder.Configuration);
 
-builder.Services.AddControllers();
-var connectionString = builder.Configuration.GetConnectionString("ApplicationContext");
-builder.Services.AddDbContext<ApplicationContext>(option => option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services
-    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(config =>
-    {
-        config.LoginPath = "/CMS_appBackend/login";
-        config.Cookie.Name = "CMS_appBackend";
-        config.LogoutPath = "/CMS_appBackend/Logout";
-    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -105,26 +50,83 @@ using (var scope = app.Services.CreateScope())
     var initializer = serviceProvider.GetRequiredService<DatabaseInitializer>();
     await initializer.Initialize().ConfigureAwait(false);
 }
+
 app.Run();
 
-// builder.Services.AddScoped<IAdminService, AdminService>();
-// builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+{
+    services.AddCors(
+        a =>
+            a.AddPolicy(
+                "CorsPolicy",
+                b =>
+                {
+                    b.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
+                }
+            )
+    );
 
-// builder.Services.AddScoped<IBlogService, BlogService>();
-// builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+    services.AddScoped<IAdminService, AdminService>();
+    services.AddScoped<IAdminRepository, AdminRepository>();
 
-// builder.Services.AddScoped<ICommentService, CommentService>();
-// builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+    services.AddScoped<IBlogService, BlogService>();
+    services.AddScoped<IBlogRepository, BlogRepository>();
 
-// builder.Services.AddScoped<ICategoryService, CategoryService>();
-// builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+    services.AddScoped<ICommentService, CommentService>();
+    services.AddScoped<ICommentRepository, CommentRepository>();
 
-// builder.Services.AddScoped<IPostService, PostService>();
-// builder.Services.AddScoped<IPostRepository, PostRepository>();
+    services.AddScoped<ICategoryService, CategoryService>();
+    services.AddScoped<ICategoryRepository, CategoryRepository>();
 
-// builder.Services.AddScoped<IUserService, UserService>();
-// builder.Services.AddScoped<IUserRepository, UserRepository>();
+    services.AddScoped<ICustomerRepository, CustomerRepository>();
+    services.AddScoped<ICustomerService, CustomerService>();
 
+    services.AddScoped<IPostService, PostService>();
+    services.AddScoped<IPostRepository, PostRepository>();
 
-builder.Services.AddScoped<IRoleService, RoleService>();
-builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+    services.AddScoped<IUserService, UserService>();
+    services.AddScoped<IUserRepository, UserRepository>();
+
+    services.AddScoped<IRoleService, RoleService>();
+    services.AddScoped<IRoleRepository, RoleRepository>();
+
+    services.AddScoped<IEmailSender, EmailSender>();
+
+    services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+    services.AddScoped<DatabaseInitializer>();
+    services.AddHttpContextAccessor();
+
+    // Add services to the container.
+    services.AddControllers();
+
+    var connectionString = configuration.GetConnectionString("ApplicationContext");
+    services.AddDbContext<ApplicationContext>(option => option.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
+
+        // Set the schemes to include "https"
+        c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+        {
+            // Your security scheme configuration...
+        });
+
+        c.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            // Your security requirements...
+        });
+
+        c.Schemes = new List<OpenApiSchema> { new OpenApiSchema { Type = "https" }, new OpenApiSchema { Type = "http" } };
+    });
+
+    services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(config =>
+        {
+            config.LoginPath = "/CMS_appBackend/login";
+            config.Cookie.Name = "CMS_appBackend";
+            config.LogoutPath = "/CMS_appBackend/Logout";
+        });
+}
