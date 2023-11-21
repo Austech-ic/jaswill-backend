51f34ac9bb0d71dd724aa287acb87b7907e0a5b5
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-11-21 13:15:23 +0100

    program

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ using CMS_appBackend.Email;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using CMS_appBackend.Identity;
-using Npgsql.EntityFrameworkCore.PostgreSQL;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Rewrite;
 using System.Data;
@@ -110,3 +109,24 @@ using (var scope = app.Services.CreateScope())
 }
 app.Run();
 
+// builder.Services.AddScoped<IAdminService, AdminService>();
+// builder.Services.AddScoped<IAdminRepository, AdminRepository>();
+
+// builder.Services.AddScoped<IBlogService, BlogService>();
+// builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+
+// builder.Services.AddScoped<ICommentService, CommentService>();
+// builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+
+// builder.Services.AddScoped<ICategoryService, CategoryService>();
+// builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+// builder.Services.AddScoped<IPostService, PostService>();
+// builder.Services.AddScoped<IPostRepository, PostRepository>();
+
+// builder.Services.AddScoped<IUserService, UserService>();
+// builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
