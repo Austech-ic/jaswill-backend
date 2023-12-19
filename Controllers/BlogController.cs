51e3bbb4c511ad70e35ef5a2d26a134c94d8b532
aderibigbeolamide <aderibigbeolamide56@gmail.com>
2023-12-19 15:24:59 +0100

    updates

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -73,7 +73,7 @@ namespace CMS_appBackend.Controllers
             return Content(blog.Message);
         }
 
-        [HttpGet("GetBlogsToDisplay")]
+        [HttpGet("GetBlogsToDisplayAsync")]
         public async Task<IActionResult> GetBlogsToDisplayAsync()
         {
             var blog = await _blogService.GetBlogsToDisplayAsync();
